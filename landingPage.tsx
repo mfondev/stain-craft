d9c130a1f308d7c7eab0b04a1ea1ceabc8e35b59
Gideon Atauba <ataubagideon@gmail.com>
2025-03-23 11:21:43 +0100

    @added arrow animation

--- landingPage.tsx
+++ landingPage.tsx
@@ -5,12 +5,11 @@ import Link from "next/link";
 import { textSlide } from "@/components/animations/animation";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
+import { SlArrowRight } from "react-icons/sl";
 gsap.registerPlugin(ScrollTrigger);
 
 export default function LandingPage() {
   useEffect(() => {
-    gsap.set(".ent-1", { x: -50})
-    // gsap.set(".ent-2", { x: -50})
     textSlide();
   }, []);
 
@@ -22,20 +21,25 @@ export default function LandingPage() {
     ));
   };
 
-
   const textShift = () => {
     const tl = gsap.timeline();
-
-
-    tl.to(".ent-2", { x: 50, opacity: 0, duration: 0.3 }) 
-      .to(".ent-1", { x: 0, opacity: 1, duration: 0.3 }, "<"); 
+    tl.to(".ent-2", { x: 50, duration: 0.3 })
+      .to(".ent-1", { x: 47, duration: 0.3 }, "<")
+      .to(".arr-1", { x: 30, duration: 0.3 },"-=0.5")
+      .to(".arr-2", { x: 30, duration: 0.3 }, "-=0.5")
+      .to(".arr-3", { x: 30, duration: 0.3 }, "-=0.5")
+      .to(".arr-4", { x: 30, duration: 0.3 }, "-=0.5");
   };
 
   const textUnshift = () => {
     const tl = gsap.timeline();
 
-    tl.to(".ent-1", { x: -50, opacity: 0, duration: 0.3 }) 
-      .to(".ent-2", { x: 0, opacity: 1, duration: 0.3 }, "<"); 
+    tl.to(".ent-1", { x: -3, duration: 0.3 })
+      .to(".ent-2", { x: 0, duration: 0.3 }, "<")
+      .to(".arr-1", { x: -50, duration: 0.3 })
+      .to(".arr-2", { x: 0, duration: 0.3 }, "<")
+      .to(".arr-3", { x: 0, duration: 0.3 }, "<")
+      .to(".arr-4", { x: 0, duration: 0.3 }, "<");
   };
 
   return (
@@ -80,15 +84,31 @@ export default function LandingPage() {
         <h3 className="text-sm uppercase">CALIFORNIA, USA, 90757</h3>
         <Link
           href=""
-          className="flex items-center justify-between w-[180px] bg-[#ef4826] hover:bg-[#26ef47] text-black p-4 rounded-br-lg"
+          className="flex items-center justify-between w-[180px] bg-[#ef4826] hover:bg-[#26ef47] text-black p-4 rounded-br-lg "
           onMouseEnter={textShift}
           onMouseLeave={textUnshift}
         >
-          <div className="text-sm  uppercase font-bold w-[47px] overflow-hidde flex items-center relativ">
-            <p className="ent-1 absolute">enter</p>
-            <p className="ent-2 absolute">enter</p>
+          <div className="uppercase font-bold w-[49px] h-[20px] overflow-hidden flex items-center relative">
+            <p className="left-[-46px] ent-1 absolute text-sm ">enter</p>
+            <p className="ent-2 absolute text-sm">enter</p>
           </div>
-          <span>{">>"}</span>
+          <div
+            className=" w-[30px] h-[20px] flex items-center justify-between overflow-hidden relative"
+            
+          >
+            <div className="flex items-center justify-between w-[15px] absolute left-[-15px]">
+              <SlArrowRight className="text-sm absolut arr-1" />
+            </div>
+            <div className="flex items-center justify-between w-[15px] absolute left-[-30px] ">
+              <SlArrowRight className="text-sm  arr-2" />
+            </div>
+            <div className="flex items-center justify-between w-[15px] absolut">
+              <SlArrowRight className="text-sm  arr-3" />
+            </div>
+            <div className="flex items-center justify-between w-[15px] ">
+              <SlArrowRight className="text-sm  arr-4" />
+            </div>
+          </div>
         </Link>
       </div>
     </div>
--- src/components/animations/animation.ts
+++ src/components/animations/animation.ts
@@ -3,7 +3,6 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 export const textSlide = () => {
-
   const tl = gsap.timeline();
 
   const text1Letters = gsap.utils.toArray(".h-text1 span");
@@ -21,27 +20,3 @@ export const textSlide = () => {
   );
 };
 
-// export const textSlide = () => {
-//   const tl = gsap.timeline();
-
-//   tl.fromTo(
-//     ".h-text1",
-//     { y: 0, opacity: 1 },
-//     { y: -140, opacity: 1, duration: 0.8, stagger: 0.3, ease: "power2.out" }
-//   ).fromTo(
-//     ".h-text2",
-//     { y: 0, opacity: 0 },
-//     { y: -140, opacity: 1, duration: 0.8, stagger: 0.3, ease: "power2.out" },
-//     "<"
-//   );
-
-//   tl.fromTo(
-//     ".l-footer",
-//     {
-//       y: 100,
-//       opacity: 0,
-//     },
-//     {y:0, opacity: 1, duration: 0.8, ease: "power2.out" },
-//     "1"
-//   );
-// };
