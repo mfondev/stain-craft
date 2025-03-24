7ba8d9e212dde11e7b1b4cb24a7df3a9fa4807d1
Gideon Atauba <ataubagideon@gmail.com>
2025-03-24 09:25:38 +0100

    finished landing page transition animation

--- landingPage.tsx
+++ landingPage.tsx
@@ -1,14 +1,16 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { FormEvent, useEffect } from "react";
 import Link from "next/link";
-import { textSlide } from "@/components/animations/animation";
+import { textSlide, textUnslide } from "@/components/animations/animation";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 import { SlArrowRight } from "react-icons/sl";
+import { useRouter } from "next/navigation";
 gsap.registerPlugin(ScrollTrigger);
 
 export default function LandingPage() {
+  const router = useRouter();
   useEffect(() => {
     textSlide();
   }, []);
@@ -25,10 +27,10 @@ export default function LandingPage() {
     const tl = gsap.timeline();
     tl.to(".ent-2", { x: 50, duration: 0.3 })
       .to(".ent-1", { x: 47, duration: 0.3 }, "<")
-      .to(".arr-1", { x: 30, duration: 0.3 },"-=0.5")
-      .to(".arr-2", { x: 30, duration: 0.3 }, "-=0.5")
-      .to(".arr-3", { x: 30, duration: 0.3 }, "-=0.5")
-      .to(".arr-4", { x: 30, duration: 0.3 }, "-=0.5");
+      .to(".arr-1", { x: 15, duration: 0.3 }, "<")
+      .to(".arr-2", { x: 16, duration: 0.3 }, "<")
+      .to(".arr-3", { x: 15, duration: 0.3 }, "<")
+      .to(".arr-4", { x: 15, duration: 0.3 }, "<");
   };
 
   const textUnshift = () => {
@@ -36,81 +38,112 @@ export default function LandingPage() {
 
     tl.to(".ent-1", { x: -3, duration: 0.3 })
       .to(".ent-2", { x: 0, duration: 0.3 }, "<")
-      .to(".arr-1", { x: -50, duration: 0.3 })
+      .to(".arr-1", { x: 0, duration: 0.3 })
       .to(".arr-2", { x: 0, duration: 0.3 }, "<")
       .to(".arr-3", { x: 0, duration: 0.3 }, "<")
       .to(".arr-4", { x: 0, duration: 0.3 }, "<");
   };
 
+  const homeRoute = (e: React.FormEvent<HTMLElement>) => {
+    e.preventDefault();
+    const timeline = gsap.timeline();
+
+    const text2Letters = gsap.utils.toArray(".h-text2 span");
+
+    timeline.to(text2Letters, {
+      y: -240,
+      duration: 0.8,
+      stagger: 0.05,
+      ease: "power2.out",
+    });
+
+    timeline.to(
+      ".overlay",
+      {
+        y: 0,
+        duration: 1.5,
+        ease: "power2.inOut",
+      },
+      "-=0.5"
+    );
+    timeline.call(() => {
+      router.push("/home");
+    });
+  };
+
   return (
-    <div className="bg-black text-white h-screen px-6 pb-6 flex flex-col justify-between">
-      <div className="bg-black text-white h-screen p-6 flex flex-col justify-between">
-        <div className="flex flex-col gap-4">
-          <div className="h-[110px] overflow-hidden flex flex-col">
-            <h1 className="text-[120px] uppercase font-extrabold h-full h-text1">
-              {wrapText("Bespoke")}
-            </h1>
-            <h1 className="text-[120px] uppercase font-extrabold h-text2">
-              {wrapText("Bespoke")}
-            </h1>
+    <>
+      <div className="relative w-full h-screen overflow-hidden">
+        <div className="bg-black h-screen text-white px-6 pb-6 flex flex-col justify-between">
+          <div className="bg-black text-white h-screen p-6 flex flex-col justify-between">
+            <div className="flex flex-col gap-4">
+              <div className="h-[110px] overflow-hidden flex flex-col">
+                <h1 className="text-[120px] leading-[1] uppercase font-extrabold h-full h-text1">
+                  {wrapText("Bespoke")}
+                </h1>
+                <h1 className="text-[120px] leading-[1] uppercase font-extrabold h-text2">
+                  {wrapText("Bespoke")}
+                </h1>
+              </div>
+              <div className="h-[110px] overflow-hidden flex flex-col ml-[400px]">
+                <h1 className="text-[120px] leading-[1] uppercase font-extrabold h-full h-text1">
+                  {wrapText("Myth")}
+                </h1>
+                <h1 className="text-[120px] leading-[1] uppercase font-extrabold h-text2">
+                  {wrapText("Myth")}
+                </h1>
+              </div>
+              <div className="h-[110px] overflow-hidden flex flex-col">
+                <h1 className="text-[120px] leading-[1] uppercase font-extrabold h-full h-text1 ">
+                  {wrapText("Chaos")}
+                </h1>
+                <h1 className="text-[120px] leading-[1] uppercase font-extrabold h-text2">
+                  {wrapText("Chaos")}
+                </h1>
+              </div>
+              <div className="h-[110px] overflow-hidden flex flex-col ml-[130px]">
+                <h1 className="text-[120px] leading-[1] uppercase font-extrabold h-full h-text1">
+                  {wrapText("Maniac")}
+                </h1>
+                <h1 className="text-[120px] leading-[1] uppercase font-extrabold h-text2">
+                  {wrapText("Maniac")}
+                </h1>
+              </div>
+            </div>
           </div>
-          <div className="h-[110px] overflow-hidden flex flex-col ml-[400px]">
-            <h1 className="text-[120px] uppercase font-extrabold h-full h-text1">
-              {wrapText("Myth")}
-            </h1>
-            <h1 className="text-[120px] uppercase font-extrabold h-text2">
-              {wrapText("Myth")}
-            </h1>
-          </div>
-          <div className="h-[110px] overflow-hidden flex flex-col">
-            <h1 className="text-[120px] uppercase font-extrabold h-full h-text1">
-              {wrapText("Chaos")}
-            </h1>
-            <h1 className="text-[120px] uppercase font-extrabold h-text2">
-              {wrapText("Chaos")}
-            </h1>
-          </div>
-          <div className="h-[110px] overflow-hidden flex flex-col ml-[130px]">
-            <h1 className="text-[120px] uppercase font-extrabold h-full h-text1">
-              {wrapText("Maniac")}
-            </h1>
-            <h1 className="text-[120px] uppercase font-extrabold h-text2">
-              {wrapText("Maniac")}
-            </h1>
+          <div className="flex items-center justify-between l-footer">
+            <h3 className="text-sm uppercase">CALIFORNIA, USA, 90757</h3>
+            <Link
+              href=""
+              onClick={homeRoute}
+              className="flex items-center justify-between w-[180px] bg-[#ef4826] hover:bg-[#26ef47] text-black p-4 rounded-br-lg "
+              onMouseEnter={textShift}
+              onMouseLeave={textUnshift}
+            >
+              <div className="uppercase font-bold w-[49px] h-[20px] overflow-hidden flex items-center relative">
+                <p className="left-[-46px] ent-1 absolute text-sm ">enter</p>
+                <p className="ent-2 absolute text-sm">enter</p>
+              </div>
+              <div className=" w-[15px] h-[15px] flex items-center justify-between overflow-hidden relative">
+                <div className="flex items-center justify-between w-[15px] absolute left-[-10px]">
+                  <SlArrowRight className="text-[10px] absolu arr-1" />
+                </div>
+                <div className="flex items-center justify-between w-[15px] absolute left-[-18px] ">
+                  <SlArrowRight className="text-[10px] arr-2" />
+                </div>
+                <div className="flex items-center justify-between w-[15px] absolute left-[5px] ">
+                  <SlArrowRight className="text-[10px] arr-3" />
+                </div>
+                <div className="flex items-center justify-between w-[15px] absolute left-[-2px] ">
+                  <SlArrowRight className="text-[10px]  arr-4" />
+                </div>
+              </div>
+            </Link>
           </div>
         </div>
+
+        <div className="overlay absolute inset-0 w-full h-full bg-white z-50"></div>
       </div>
-      <div className="flex items-center justify-between l-footer">
-        <h3 className="text-sm uppercase">CALIFORNIA, USA, 90757</h3>
-        <Link
-          href=""
-          className="flex items-center justify-between w-[180px] bg-[#ef4826] hover:bg-[#26ef47] text-black p-4 rounded-br-lg "
-          onMouseEnter={textShift}
-          onMouseLeave={textUnshift}
-        >
-          <div className="uppercase font-bold w-[49px] h-[20px] overflow-hidden flex items-center relative">
-            <p className="left-[-46px] ent-1 absolute text-sm ">enter</p>
-            <p className="ent-2 absolute text-sm">enter</p>
-          </div>
-          <div
-            className=" w-[30px] h-[20px] flex items-center justify-between overflow-hidden relative"
-            
-          >
-            <div className="flex items-center justify-between w-[15px] absolute left-[-15px]">
-              <SlArrowRight className="text-sm absolut arr-1" />
-            </div>
-            <div className="flex items-center justify-between w-[15px] absolute left-[-30px] ">
-              <SlArrowRight className="text-sm  arr-2" />
-            </div>
-            <div className="flex items-center justify-between w-[15px] absolut">
-              <SlArrowRight className="text-sm  arr-3" />
-            </div>
-            <div className="flex items-center justify-between w-[15px] ">
-              <SlArrowRight className="text-sm  arr-4" />
-            </div>
-          </div>
-        </Link>
-      </div>
-    </div>
+    </>
   );
 }
--- src/components/animations/animation.ts
+++ src/components/animations/animation.ts
@@ -4,19 +4,32 @@ gsap.registerPlugin(ScrollTrigger);
 
 export const textSlide = () => {
   const tl = gsap.timeline();
-
   const text1Letters = gsap.utils.toArray(".h-text1 span");
   const text2Letters = gsap.utils.toArray(".h-text2 span");
-
   tl.fromTo(
     text1Letters,
     { y: 0, opacity: 1 },
     { y: -140, opacity: 1, duration: 0.8, stagger: 0.05, ease: "power2.out" }
-  ).fromTo(
-    text2Letters,
-    { y: 0, opacity: 0 },
-    { y: -140, opacity: 1, duration: 0.8, stagger: 0.05, ease: "power2.out" },
-    "<" 
-  );
+  )
+    .fromTo(
+      text2Letters,
+      { y: 0, opacity: 0 },
+      { y: -110, opacity: 1, duration: 0.8, stagger: 0.05, ease: "power2.out" },
+      "<"
+    )
+    .fromTo(
+      ".l-footer",
+      { y: 100, opacity: 0 },
+      { y: 0, duration: 0.8, ease: "power2.out", opacity: 1 }
+    );
 };
 
+export const textUnslide = () => {
+  const text2Letters = gsap.utils.toArray(".h-text2 span");
+  gsap.to(text2Letters, {
+    y: -140,
+    duration: 0.8,
+    stagger: 0.05,
+    ease: "power2.out",
+  });
+};
