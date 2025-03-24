198970c9e3c1135c796f2d9920d58e31ad4c9dbd
Gideon Atauba <ataubagideon@gmail.com>
2025-03-24 15:00:13 +0100

    added navbar

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,36 @@
-import React from 'react';
+"use client";
+
+import Navbar from "@/components/mainNavigation/navbar";
+import React from "react";
+import { useEffect } from "react";
+import gsap from "gsap";
+import SplitType from "split-type";
+import Link from "next/link";
 
 export default function Home() {
+  useEffect(() => {
+    const split = new SplitType(".hf", { types: "chars" });
+    console.log(split.chars);
+
+    gsap.from(split.chars, {
+      opacity: 0,
+      y: 20,
+      stagger: 1,
+      duration: 0.6,
+    });
+  }, []);
+
   return (
-    <div>
-      home page 
-    </div>
+    <>
+      <div className="bg-[#eaece9] h-screen">
+        <Navbar />
+        <h1 className="hf text-[200px] font-extrabold ">HF</h1>
+      </div>
+      <main>
+        <h2>Less is more,</h2>
+        <h2>more is less.</h2>
+        <Link href="">watch video</Link>
+      </main>
+    </>
   );
 }
