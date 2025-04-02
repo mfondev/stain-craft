3e25b9d8728f2e0fb483012d6ac159405cec0736
Gideon Atauba <ataubagideon@gmail.com>
2025-04-02 21:39:47 +0100

    fixed clip path error

--- src/components/animations/animation.ts
+++ src/components/animations/animation.ts
@@ -105,20 +105,28 @@ export const homeAnimation = () => {
 
 const onGoingCode = (progress: number) => {
   gsap.to(".carImg", {
-    scale: 1.2 * (1 - progress), 
+    scale: 1.2 * (1 - progress),
     duration: 1,
     ease: "power2.out",
     scrub: 1,
   });
 
-  const maxMove = -355; 
+  const maxMove = -355;
 
   gsap.to(".eleven", {
-    y: Math.max(maxMove, -700 * progress), 
+    y: Math.max(maxMove, -700 * progress),
     ease: "power2.out",
     duration: 1,
     marginBottom: 10,
   });
+
+  // gsap.fromTo(".panel_1",{
+  //   filter: "brightness(0.5)",
+  //   duration: 1,
+  //   ease: "power2.out",
+  // },{
+  //   filter: `brightness(${0.5 + progress})`,
+  // })
 };
 
 export const createScroll01 = () => {
@@ -139,10 +147,33 @@ export const createScroll01 = () => {
       .to(
         panel,
         {
-          // filter: isLast ? "none" : "brightness(50%) blur(10px)",
+          // filter: "brightness(50%) blur(10px)",
           ease: "expo.inOut",
         },
         "<"
       );
   });
+};
+
+export const scaleAnimation = () => {
+  const timeline = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".main-section",
+      start: "top bottom",
+      scrub: true,
+      markers: true,
+    },
+  });
+
+  timeline
+    .to(".scale-section", {
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      ease: "power2.out",
+      duration: 3,
+    })
+    .fromTo(
+      ".backgroundImg",
+      { width: "100%" },
+      { width: "50%", duration: 1, ease: "power2.out" }
+    );
 };
--- src/components/homeHero.tsx
+++ src/components/homeHero.tsx
@@ -2,16 +2,76 @@
 
 import React, { useEffect } from "react";
 import gsap from "gsap";
+import Image from "next/image";
 import ScrollTrigger from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
+import { scaleAnimation } from "./animations/animation";
 
 export default function HomeHero() {
+  useEffect(() => {
+    scaleAnimation();
+  }, []);
 
   return (
     <>
       <main className="relative">
-        <section className="min-h-[100vh] bg-black rounded-t-[50px] car-sectio sticky top-0 panel"></section>
-        <section className="min-h-[200vh] bg-amber-800 rounded-t-[50px sticky top-0 panel"></section>
+        <section className="min-h-[100vh] bg-black rounded-t-[50px] car-sectio sticky top-0 panel flex brightness-75 panel_1 ">
+          <div className="relative w-1/2">
+            <Image
+              src="/images/koz_1.jpg"
+              alt=""
+              fill
+              className="object-cover rounded-tl-[50px]"
+            />
+          </div>
+          <div className="w-[1px] bg-[#c4c0c0]"></div>
+          <div className="relative w-1/2">
+            <Image
+              src="/images/koz_2.jpg"
+              alt=""
+              fill
+              className="object-cover rounded-tr-[50px]"
+            />
+            <div className=" absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-whit z-20 flex flex-col gap-[200px]">
+              <span>
+                <h1 className="uppercase text-2xl text-left font-extrabold">
+                  staincraft
+                </h1>
+                <p className="uppercase text-2xl text-left font-extrabold">
+                  where the past overtakes the future
+                </p>
+              </span>
+              <p className="uppercase text-2xl text-left font-extrabold">
+                Hear sound
+              </p>
+            </div>
+          </div>
+        </section>
+        <section className="h-[250vh] rounded-t-[50px] sticky top-0 panel">
+          <div
+            className=" relative h-[150vh]
+          "
+          >
+            <Image
+              src="/images/koz_1.jpg"
+              alt=""
+              fill
+              className="object-cover rounded-tr-[50px]"
+            />
+          </div>
+          <main className="bg-[#eaece9] main-section">
+            <section className=" h-[100vh] relative flex scale-section">
+              <div className="absolute bg-amber-400 right-0 top-0 w-1/2 h-full"></div>
+              <div className="absolute inset-0 backgroundImg">
+                <ul className="z-50 text-white place-items-cente h-[100vh] flex flex-col justify-center items-center">
+                  <li className="uppercase font-bold text-2xl">Maniacs</li>
+                  <li className="uppercase font-bold text-2xl">Mission</li>
+                  <li className="uppercase font-bold text-2xl">Machine</li>
+                </ul>
+              </div>
+            </section>
+          </main>
+        </section>
       </main>
     </>
   );
